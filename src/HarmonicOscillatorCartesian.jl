532232dfd5762646cba8fa16eade3d7d0b6faadb
Chris Bradly <christof.bradly@gmail.com>
2023-01-25 14:07:21 +1300

    deps and formatting

--- src/HarmonicOscillatorCartesian.jl
+++ src/HarmonicOscillatorCartesian.jl
@@ -1,15 +1,18 @@
 module HarmonicOscillatorCartesian
-    using Rimu
-    using Rimu.Hamiltonians: num_singly_doubly_occupied_sites, AbstractOffdiagonals, BoseOccupiedModeMap
-    import Rimu.Interfaces: starting_address, num_offdiagonals, get_offdiagonal, offdiagonals, diagonal_element
-    using StaticArrays
-    using LinearAlgebra
-    using SpecialFunctions
-    using DataFrames, Combinatorics
 
-    export HarmonicOscillatorWeak
-    export get_all_blocks
+using StaticArrays
+using LinearAlgebra
+using SpecialFunctions
+using DataFrames, Combinatorics
 
-    include("HarmonicOscillatorWeak.jl")
-    include("blocks.jl")
+using Rimu
+using Rimu.Hamiltonians: num_singly_doubly_occupied_sites, AbstractOffdiagonals, BoseOccupiedModeMap
+import Rimu.Interfaces: starting_address, num_offdiagonals, get_offdiagonal, offdiagonals, diagonal_element
+
+export HarmonicOscillatorWeak
+export get_all_blocks
+
+include("HarmonicOscillatorWeak.jl")
+include("blocks.jl")
+
 end

9ae5f1409c3edc81582d3e57ba6fbf19896ad04c
Chris Bradly <christof.bradly@gmail.com>
2023-02-15 15:24:01 +1300

    BSR basis-only

--- src/HarmonicOscillatorCartesian.jl
+++ src/HarmonicOscillatorCartesian.jl
@@ -7,12 +7,13 @@ using DataFrames, Combinatorics
 
 using Rimu
 using Rimu.Hamiltonians: num_singly_doubly_occupied_sites, AbstractOffdiagonals, BoseOccupiedModeMap
-import Rimu.Interfaces: starting_address, num_offdiagonals, get_offdiagonal, offdiagonals, diagonal_element
+import Rimu.Interfaces: starting_address, num_offdiagonals, get_offdiagonal, offdiagonals, diagonal_element, LOStructure
 
 export HarmonicOscillatorWeak
 export get_all_blocks, fock_to_cartHO_basis
 
 include("HarmonicOscillatorWeak.jl")
+include("basis-only-bsr.jl")
 include("vertices.jl")
 include("blocks.jl")
 
--- src/basis-only-bsr.jl
+++ src/basis-only-bsr.jl
@@ -1,4 +1,5 @@
-import Rimu.Hamiltonians: _bsr_ensure_symmetry, check_address_type, fix_approx_hermitian!
+using Rimu.Hamiltonians: check_address_type, fix_approx_hermitian!, build_sparse_matrix_from_LO
+import Rimu.Hamiltonians: BasisSetRep, _bsr_ensure_symmetry
 
 function BasisSetRep(h::HarmonicOscillatorWeak, addr=starting_address(h); basis_only = false, kwargs...)
     return _bsr_ensure_symmetry(LOStructure(h), h, addr; basis_only, kwargs...)
@@ -11,9 +12,9 @@ function _bsr_ensure_symmetry(
     dimension(Float64, h) < sizelim || throw(ArgumentError("dimension larger than sizelim"))
     check_address_type(h, addr)
     if basis_only
-        sm, basis = build_sparse_matrix_from_LO(h, addr; kwargs...)
-    else
         sm, basis = build_sparse_matrix_from_LO_basis_only(h, addr; kwargs...)
+    else
+        sm, basis = build_sparse_matrix_from_LO(h, addr; kwargs...)
         fix_approx_hermitian!(sm; test_approx_symmetry) # enforce hermitian symmetry after building
     end    
     return BasisSetRep(sm, basis, h)
@@ -39,21 +40,21 @@ function build_sparse_matrix_from_LO_basis_only(
     col = Dict{Int,T}()       # Temporary column storage
     sizehint!(col, num_offdiagonals(ham, address))
 
-    is = Int[] # row indices
-    js = Int[] # column indice
-    vs = T[]   # non-zero values
+    # is = Int[] # row indices
+    # js = Int[] # column indice
+    # vs = T[]   # non-zero values
 
-    sizehint!(is, nnzs)
-    sizehint!(js, nnzs)
-    sizehint!(vs, nnzs)
+    # sizehint!(is, nnzs)
+    # sizehint!(js, nnzs)
+    # sizehint!(vs, nnzs)
 
     i = 0
     while i < length(adds)
         i += 1
         add = adds[i]
-        push!(is, i)
-        push!(js, i)
-        push!(vs, diagonal_element(ham, add))
+        # push!(is, i)
+        # push!(js, i)
+        # push!(vs, diagonal_element(ham, add))
 
         empty!(col)
         for (off, v) in offdiagonals(ham, add)
@@ -75,19 +76,19 @@ function build_sparse_matrix_from_LO_basis_only(
             end
         end
         # Copy the column into the sparse matrix vectors.
-        for (j, v) in col
-            iszero(v) && continue
-            push!(is, i)
-            push!(js, j)
-            push!(vs, v)
-        end
+        # for (j, v) in col
+        #     iszero(v) && continue
+        #     push!(is, i)
+        #     push!(js, j)
+        #     push!(vs, v)
+        # end
     end
 
-    matrix = sparse(js, is, vs, length(adds), length(adds))
+    # matrix = sparse(js, is, vs, length(adds), length(adds))
     if sort
         perm = sortperm(adds; kwargs...)
-        return permute!(matrix, perm, perm), permute!(adds, perm)
+        return nothing, permute!(adds, perm)
     else
-        return matrix, adds
+        return nothing, adds
     end
 end

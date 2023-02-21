42809db1e3a3ef94ef3bdff827a6d0c24b9faba1
Chris Bradly <christof.bradly@gmail.com>
2023-02-22 11:12:54 +1300

    simplify basis-only

--- src/HarmonicOscillatorCartesian.jl
+++ src/HarmonicOscillatorCartesian.jl
@@ -7,7 +7,7 @@ using DataFrames, Combinatorics
 
 using Rimu
 using Rimu.Hamiltonians: num_singly_doubly_occupied_sites, AbstractOffdiagonals, BoseOccupiedModeMap
-import Rimu.Interfaces: starting_address, num_offdiagonals, get_offdiagonal, offdiagonals, diagonal_element, LOStructure
+import Rimu.Interfaces: starting_address, num_offdiagonals, get_offdiagonal, offdiagonals, diagonal_element
 
 export HarmonicOscillatorWeak
 export get_all_blocks, fock_to_cartHO_basis
--- src/basis-only-bsr.jl
+++ src/basis-only-bsr.jl
@@ -1,27 +1,9 @@
-using Rimu.Hamiltonians: check_address_type, build_sparse_matrix_from_LO, LOStructure
-import Rimu.Hamiltonians: _bsr_ensure_symmetry
-# using SparseArrays: nnz, spzeros
-
-function _bsr_ensure_symmetry(
-    ::LOStructure, h::HarmonicOscillatorWeak, addr;
-    sizelim=10^6, test_approx_symmetry=true, basis_only = false, kwargs...
-)
-    dimension(Float64, h) < sizelim || throw(ArgumentError("dimension larger than sizelim"))
-    check_address_type(h, addr)
-    if basis_only
-        sm, basis = build_basis_only_from_LO(h, addr; kwargs...)
-    else
-        sm, basis = build_sparse_matrix_from_LO(h, addr; kwargs...)
-    end  
-    return BasisSetRep(sm, basis, h)
-end
-
 # calculate the basis without storing the sparse matrix
+# remove once this is added to Rimu
 function build_basis_only_from_LO(
-    ham, address=starting_address(ham);
+    ham::HarmonicOscillatorWeak, address=starting_address(ham);
     cutoff=nothing,
     filter=isnothing(cutoff) ? nothing : (a -> diagonal_element(ham, a) ≤ cutoff),
-    nnzs=dimension(ham),
     sort=false, kwargs...,
 )
     if !isnothing(filter) && !filter(address)
@@ -30,18 +12,14 @@ function build_basis_only_from_LO(
             "Please pick a different address or a different filter."
         )))
     end
-    T = eltype(ham)
     adds = [address]          # Queue of addresses. Also returned as the basis.
     dict = Dict(address => 1) # Mapping from addresses to indices
-    col = Dict{Int,T}()       # Temporary column storage
-    sizehint!(col, num_offdiagonals(ham, address))
 
     i = 0
     while i < length(adds)
         i += 1
         add = adds[i]
 
-        empty!(col)
         for (off, v) in offdiagonals(ham, add)
             iszero(v) && continue
             j = get(dict, off, nothing)
@@ -56,16 +34,13 @@ function build_basis_only_from_LO(
                     dict[off] = j
                 end
             end
-            if !iszero(j)
-                col[j] = get(col, j, zero(T)) + v
-            end
         end
     end
 
     if sort
         perm = sortperm(adds; kwargs...)
-        return nothing, permute!(adds, perm)
+        return permute!(adds, perm)
     else
-        return nothing, adds
+        return adds
     end
 end
